508caa55e9607bb0db72cc1581525b2fa521ef5c
Fajar Sujito <fajar@dxtr.asia>
2020-09-12 09:15:58 +0700

    add security helpers

---
+++ src/Helpers/SecurityHelpers.php
@@ -0,0 +1,58 @@
+<?php
+
+if (function_exists('ayoencrypt')) {
+    /**
+     * if function exixts
+     *
+     * @param  string  $value
+     * @return string
+     */
+    return  ayoencrypt($value);
+} else {
+    /**
+     * Encrypt data
+     *
+     * @param  string  $value
+     * @return string
+     */
+    function ayoencrypt($value)
+    {
+        dd('t');
+        $key   = hash('sha256', config('app.key'));
+        $iv    = substr($key, 0, openssl_cipher_iv_length(config('app.cipher')));
+
+        $value = serialize($value);
+        $value = openssl_encrypt($value, config('app.cipher'), $key, 0, $iv);
+        $value = base64_encode($value);
+
+        return $value;
+    }
+}
+
+if (function_exists('ayodecrypt')) {
+    /**
+     * if function exixts
+     *
+     * @param  string  $value
+     * @return string
+     */
+    return  ayodecrypt($value);
+} else {
+    /**
+     * Decrypt data
+     *
+     * @param  string  $value
+     * @return string
+     */
+    function ayodecrypt($value)
+    {
+        $key   = hash('sha256', config('app.key'));
+        $iv    = substr($key, 0, openssl_cipher_iv_length(config('app.cipher')));
+
+        $value = base64_decode($value);
+        $value = openssl_decrypt($value, config('app.cipher'), $key, 0, $iv);
+        $value = unserialize($value);
+
+        return $value;
+    }
+}
